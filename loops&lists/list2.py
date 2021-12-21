ff893d4080b6fc9c7cdb8f777fe20745ee2f46c1
kibet kilel <amos.rotich@strathmore.edu>
2021-12-21 11:25:35 +0300

    Add something amazing

--- loops&lists/list2.py
+++ loops&lists/list2.py
@@ -1,7 +1,10 @@
 list = ['physics', 'chemistry', 1997, 2000]
-print ("Value available at index 2 : ", list[2])
+print("Value available at index 2 : ", list[2])
 
 list[2] = 2001
-print ("New value available at index 2 : ", list[2])
+print("New value available at index 2 : ", list[2])
 list[3] = 3000
-print ("New value is ", list[3])
+print("New value is ", list[3])
+
+del list[2]
+print("After deleting value at index 2 : ", list)
