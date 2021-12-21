5fd399f1a6552eeadf43ea3d6965982d6ec659ba
kibet kilel <amos.rotich@strathmore.edu>
2021-12-21 20:38:55 +0300

    Add something amazing

--- hallo.py
+++ hallo.py
@@ -9,4 +9,10 @@ print(len(myName))
 print('What is your age?')
 # ask for their age
 myAge = input()
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+print('You will be ' + str(int(myAge) + 5) + ' in 5 years.')
+
+
+# Value that is passed to a function call is an argument .
+# Notice that the quotes are not printed to the screen.
+# They just mark where the string begins and ends; they
+# are not part of the string value.
---
+++ iteration.py
@@ -0,0 +1,8 @@
+name = input("Input your name ")
+password = input('Print your password')
+if name == 'Kile':
+    print('Hello'+ name)
+if password == 'swordfish':
+    print('Access granted.')
+else:
+    print('Wrong password.')
