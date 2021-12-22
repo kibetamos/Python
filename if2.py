b043b426f08ae754b1fa8486fbdad1171aa06f90
kibet kilel <amos.rotich@strathmore.edu>
2021-12-22 12:47:03 +0300

    Add something Amazing

---
+++ if2.py
@@ -0,0 +1,7 @@
+name = input('Whats your name')
+age = input('And whats your age')
+
+if name == 'Alice':
+    print ('Hi, Alice.')
+elif age < 12:
+    print('You are neither Alice nor a little kid')
